fc3debad961b8ab0536e2063f8a3c5473e2b9c3b
vitaluha <vmatiyash@gmail.com>
2018-02-10 19:36:36 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Currency.create(
+ team_market_percent: '5%',
+ ico_date: '2017-02-10 20:58:21',
+ vertical: 'Utilities',
+ team: 'Doge2.0',
+ team_rating: '5',
+ needs_blockchain: 'yes',
+ countries_ico_available: 'USA, Canada',
+ scam_sale_probability: '1%',
+ smart_money_investors: 'No',
+ mining_difficulty: nil,
+ arbitrage_activity: nil,
+ created_at: '2015-02-10 20:58:21',
+ updated_at: '2018-02-10 20:58:21',
+ name: 'DogeCoin',
+ json_data: nil,
+ link_to_wp: 'www.dogecoin.com',
+ number_of_coins_issued: '113146970858',
+ coin_release_date: nil,
+ whitepaper_link: 'www.dogecoin.com',
+ coins_issued: '113146970858',
+ coin_image: nil,
+ logo: nil
+    )
+
+Currency.create(
+ team_market_percent: '0.21%',
+ ico_date: '2017-01-11 20:58:21',
+ vertical: 'Animals',
+ team: 'Catturri',
+ team_rating: '1',
+ needs_blockchain: 'No',
+ countries_ico_available: 'China, South Korea',
+ scam_sale_probability: '80%',
+ smart_money_investors: 'No',
+ mining_difficulty: nil,
+ arbitrage_activity: nil,
+ created_at: '2015-12-10 20:18:21',
+ updated_at: '2018-02-10 20:58:21',
+ name: 'Cat Coins',
+ json_data: nil,
+ link_to_wp: 'www.dogecoin.com',
+ number_of_coins_issued: '113146970858',
+ coin_release_date: nil,
+ whitepaper_link: 'www.catcoins.co',
+ coins_issued: '9000000',
+ coin_image: nil,
+ logo: nil
+    )
